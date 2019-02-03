e3ca22f6b796f544eea65b2ec312ea8ffa7374e4
Otmar Humbel <ohumbel@gmail.com>
2019-02-03 10:38:47 +0100

    add the possibility to log the environment

--- root/src/org/jibble/logbot/LogBotMain.java
+++ root/src/org/jibble/logbot/LogBotMain.java
@@ -5,6 +5,8 @@ import java.util.*;
 
 public class LogBotMain {
 
+	private static final boolean DEBUG = false;
+	
     public static void main(String[] args) throws Exception {
         String rootDir;
         if (args != null && args.length == 1) {
@@ -12,6 +14,8 @@ public class LogBotMain {
         } else {
             rootDir = ".";
         }
+        log("root dir (expected subdir /html): " + rootDir);
+        
         Properties p = new Properties();
         p.load(new FileInputStream(new File(rootDir.concat("/config.ini"))));
         String server = p.getProperty("Server", "localhost");
@@ -19,12 +23,20 @@ public class LogBotMain {
         String nick = p.getProperty("Nick", "LogBot");
         String pass = p.getProperty("Pass", "thePassword");
         String joinMessage = p.getProperty("JoinMessage", "This channel is logged.");
+        log("server: " + server);
+        log("channel: " + channel);
+        log("nick: " + nick);
+        log("pass: " + pass);
+        log("joinMessage: " + joinMessage );
+        
         File outDir = new File(p.getProperty("OutputDir", rootDir.concat("/output/")));
         outDir.mkdirs();
         if (!outDir.isDirectory()) {
             System.out.println("Cannot make output directory (" + outDir + ")");
             System.exit(1);
         }
+        log("outDir: " + outDir );
+        
         LogBot.copy(new File(rootDir.concat("/html/header.inc.php")), new File(outDir, "header.inc.php"));
         LogBot.copy(new File(rootDir.concat("/html/footer.inc.php")), new File(outDir, "footer.inc.php"));
         LogBot.copy(new File(rootDir.concat("/html/index.php")), new File(outDir, "index.php"));
@@ -40,8 +52,16 @@ public class LogBotMain {
         writer.write("?>");
         writer.flush();
         writer.close();
+        
         LogBot bot = new LogBot(nick, outDir, joinMessage);
         bot.connect(server, pass);
         bot.joinChannel(channel);
     }
+    
+    private static void log(String msg) {
+    	if(DEBUG) {
+    		System.out.println(msg);
+    	}
+    }
+    
 }
