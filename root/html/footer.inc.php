43550ec62d9bf84da03fc42a2faa6e9e266364a2
Otmar Humbel <ohumbel@gmail.com>
2019-02-07 18:24:11 +0100

    add a footer reference to the gitub project

--- root/html/footer.inc.php
+++ root/html/footer.inc.php
@@ -1,7 +1,8 @@
 <p>
  These logs were automatically created by <b><?php echo($nick); ?></b> on
  <a href="irc://<?php echo($server . "/" . substr($channel, 1)); ?>"><?php echo($server); ?></a>
- using a slightly modified version of the <a href="http://www.jibble.org/logbot/">Java IRC LogBot</a>.
+ using a slightly modified version of the <a href="http://www.jibble.org/logbot/">Java IRC LogBot</a>
+ (<a href="https://github.com/ohumbel/jython-logbot">github</a>).
 </p>
 
 </body>
